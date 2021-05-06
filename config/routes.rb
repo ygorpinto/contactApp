72c6f18d8c0713e7683d83b4136bbab102a53dc5
ygorpinto <ygor.pinto@hotmail.com>
2021-05-06 17:10:20 -0300

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   resources :contacts
   root 'home#index'
   get '/home/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
